152d591fe70dc1a7138a7e2b706f0a5fd05b28b2
nabs-grg <narbu.grg@gmail.com>
2017-01-14 23:45:06 +0000

    folder structure for pig-game and used grunt as build tool

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+
+	// Project configuration.
+	grunt.initConfig({
+		concat: {
+			js: {
+				src: ['js/app.js',],
+				dest: 'build/js/script.js',
+			},
+			css: {
+				src: ['css/main.css',],
+				dest: 'build/css/styles.css',
+			},
+			},
+
+
+			watch: {
+			js: {
+				files: ['js/**/*.js'],
+				tasks: ['concat:js'],
+			},
+			css: {
+				files: ['css/**/*.css'],
+				tasks: ['concat:css'],
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['concat','watch']);
+
+};
